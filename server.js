c7b5484f1de74cc6268feba829ed5979b79b7624
TJ <tj@ojingolabs.com>
2013-09-04 18:49:36 +0400

    handle_incoming_requests and start the server and listen

--- server.js
+++ server.js
@@ -5,3 +5,14 @@
  * Time: 6:44 PM
  * Author:  TJ Marbois
  */
+var http = require('http');
+
+function handle_incoming_request(req, res) {
+    console.log("INCOMING REQUEST: " + req.method + " " + req.url);
+    res.writeHead(200, {"Content-Type" : "application/json"});
+    res.end(JSON.stringify( { error: null }) + "\n");
+}
+
+var s = http.createServer(handle_incoming_request);
+s.listen(8080);
+
